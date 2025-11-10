3f24c1d9e85b3a43e11524593127d009f06098e1
Erik Bratton <erikbratton@gmail.com>
2025-11-09 23:57:11 -0500

    Added in client code. Works but need to make it so when I print text it prints out nicer instead of how it is right now where it essential looks like the returned text is the text you entered.

---
+++ client.py
@@ -0,0 +1,46 @@
+from email import message
+from multiprocessing.dummy.connection import Client
+from socket import *
+import threading
+
+# Send a message
+def messageSendHandler(message, clientSocket):
+    while True:
+        message = input("Input your message:")
+        clientSocket.send(message.encode())
+
+# Receive a message
+def messageReceiveHandler(clientSocket):
+    while True:
+        receivedMessage = clientSocket.recv(1024).decode()
+
+        if len(receivedMessage) == 0:
+            break
+
+        print(f"\n{receivedMessage}")
+
+
+# Want to essentially create a message sender and a message receiver
+# We will have a thread to manage both
+serverIp = ""
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverIp, serverPort))
+print(f"Client Ip: {clientSocket.getsockname()[0]} \n Client Port: {clientSocket.getsockname()[1]}")
+
+# Create threads
+sendThread = threading.Thread(target=messageSendHandler, args = (message, clientSocket), daemon = True)
+receiveThread = threading.Thread(target = messageReceiveHandler, args = (clientSocket,), daemon = True)
+
+# Start threads
+sendThread.start()
+receiveThread.start()
+
+# Keep main thread running while the other threads do their work
+try:
+    sendThread.join()
+    receiveThread.join()
+except KeyboardInterrupt:
+    print("\nClient shutting down...")
+    clientSocket.close()
